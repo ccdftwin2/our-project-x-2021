ebb93139fb8c8bec1dd93ab201418cf5aaecd8d9
hlin324 <hlin324@submit-1.chtc.wisc.edu>
2021-12-30 22:20:42 -0600

    change NN size

--- models/Linear_Regularized_Model.py
+++ models/Linear_Regularized_Model.py
@@ -15,13 +15,13 @@ class toy_model:
     
         # Encoder gene expression data
         gene_encoder = layers.Input(shape=self.gene_input_shape, name='gene_input')
-        gene_d1 = layers.Dense(5000, activation='relu',name='gene_d1',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_encoder)
+        gene_d1 = layers.Dense(30000, activation='relu',name='gene_d1',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_encoder)
         gene_d1 = layers.Dropout(0.3)(gene_d1)
-        gene_d2 = layers.Dense(1000, activation='relu',name='gene_d2',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d1)
+        gene_d2 = layers.Dense(5000, activation='relu',name='gene_d2',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d1)
         gene_d2 = layers.Dropout(0.3)(gene_d2)
-        gene_d3 = layers.Dense(100, activation='relu',name='gene_d3',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d2)
+        gene_d3 = layers.Dense(1000, activation='relu',name='gene_d3',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d2)
         gene_d3 = layers.Dropout(0.3)(gene_d3)
-        gene_d4 = layers.Dense(50, activation='relu',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d3)
+        gene_d4 = layers.Dense(100, activation='relu',kernel_regularizer=tf.keras.regularizers.L2(0.03))(gene_d3)
         output = layers.Dense(1, activation='relu')(gene_d4)
         
         model = tf.keras.Model(inputs=[gene_encoder], outputs=[output])
